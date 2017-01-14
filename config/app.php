80de5f26ca5b4b77be7df061401e6ba359b00afd
Varun Mathur <vkmathur97@yahoo.co.uk>
2017-01-14 12:27:16 +0000

    change app name

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'ur mum',
+    'name' => 'comp205p',
 
     /*
     |--------------------------------------------------------------------------
